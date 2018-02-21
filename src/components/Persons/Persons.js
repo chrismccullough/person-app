d5715ce2da2ae69b5eb7784e21afeb7eb9cc2e94
Chris McCullough <cmccullough@gmail.com>
2018-02-20 21:18:58 -0800

    Added a ref

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -49,9 +49,10 @@ class Persons extends PureComponent {
 		return this.props.persons.map( (person, index) => {
 			return <Person 
 				click={() => this.props.clicked(index)}
-				key={person.id}
+				position={index}
 				name={person.name} 
 				age={person.age} 
+				key={person.id} 
 				changed ={(event) => this.props.changed(event, person.id)} />
 		} );
 	}
